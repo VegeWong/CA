bff1bf40e7aaf2f73b544d20f3aea4fdf8998043
Vegewong <wanghanjingwhj@gmail.com>
2017-12-31 21:25:07 +0800

    Stuck in ex.v

--- ex.v
+++ ex.v
@@ -1,54 +1,27 @@
-//////////////////////////////////////////////////////////////////////
-////                                                              ////
-//// Copyright (C) 2014 leishangwen@163.com                       ////
-////                                                              ////
-//// This source file may be used and distributed without         ////
-//// restriction provided that this copyright statement is not    ////
-//// removed from the file and that any derivative work contains  ////
-//// the original copyright notice and the associated disclaimer. ////
-////                                                              ////
-//// This source file is free software; you can redistribute it   ////
-//// and/or modify it under the terms of the GNU Lesser General   ////
-//// Public License as published by the Free Software Foundation; ////
-//// either version 2.1 of the License, or (at your option) any   ////
-//// later version.                                               ////
-////                                                              ////
-//// This source is distributed in the hope that it will be       ////
-//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
-//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
-//// PURPOSE.  See the GNU Lesser General Public License for more ////
-//// details.                                                     ////
-////                                                              ////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-// Module:  ex
-// File:    ex.v
-// Author:  Lei Silei
-// E-mail:  leishangwen@163.com
-// Description: ִ�н׶�
-// Revision: 1.0
-//////////////////////////////////////////////////////////////////////
-
 `include "defines.v"
 
 module ex(
 
-	input wire										rst,
+	input wire					  rst,
 	
-	//�͵�ִ�н׶ε���Ϣ
-	input wire[`AluOpBus]         aluop_i,
-	input wire[`AluSelBus]        alusel_i,
+	//送到执行阶段的信息
+	input wire[`OpcodeBus]        opcode_i,
+	input wire[`Func3Bus]         func3_i,
+	input wire[`Func7Bus]         func7_i,
 	input wire[`RegBus]           reg1_i,
 	input wire[`RegBus]           reg2_i,
+	input wire[`InstAddrBus]      pc_i,
 	input wire[`RegAddrBus]       wd_i,
 	input wire                    wreg_i,
 
-	
+	//送到Mem阶段信息
 	output reg[`RegAddrBus]       wd_o,
 	output reg                    wreg_o,
-	output reg[`RegBus]						wdata_o
-	
+	output reg[`RegBus]			  wdata_o,
+
+	output reg					  br_o; //branch result
+	output reg				      
+
 );
 
 	reg[`RegBus] logicout;
@@ -56,14 +29,192 @@ module ex(
 		if(rst == `RstEnable) begin
 			logicout <= `ZeroWord;
 		end else begin
-			case (aluop_i)
-				`EXE_OR_OP:			begin
-					logicout <= reg1_i | reg2_i;
+			case (opcode_i)
+		  		`OP_LUI: begin                       
+					logicout <= reg1_i;
+		  		end
+				`OP_AUIPC: begin
+					logicout <= reg1_i;
 				end
-				default:				begin
-					logicout <= `ZeroWord;
+				`OP_JAL: begin                       
+					logicout <= reg1_i;
+		  		end
+				`OP_JALR: begin
+					logicout <= reg2_i;
 				end
-			endcase
+				`OP_BRANCH: begin
+					case (func3_i)
+						`FUNCT3_BEQ: begin
+							br_o <= (reg1_i == reg2_i?) 1:0;
+						end
+						`FUNCT3_BNE: begin
+							
+						end
+						`FUNCT3_BLT: begin
+							
+						end
+						`FUNCT3_BGE: begin
+							
+						end
+						`FUNCT3_BLTU: begin
+							
+						end
+						`FUNCT3_BGEU: begin
+							
+						end
+					endcase
+		  		end
+				`OP_LOAD: begin
+					wreg_o <= `WriteEnable; instvalid = `InstValid;
+					imm <= {20{inst_i[31]}, inst_i[30:20]};
+					opcode_o <= `OP_LOAD; wd_o <= inst_i[11:7];
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b0;  
+					case (op2)
+						`FUNCT3_LB: begin
+							func3_o <= `FUNCT3_LB;
+						end
+						`FUNCT3_LH: begin
+							func3_o <= `FUNCT3_LH;
+						end
+						`FUNCT3_LW: begin
+							func3_o <= `FUNCT3_LW;
+						end
+						`FUNCT3_LBU: begin
+							func3_o <= `FUNCT3_LBU;
+						end
+						`FUNCT3_LHU: begin
+							func3_o <= `FUNCT3_LHU;
+						end
+					endcase
+				end
+				`OP_STORE: begin
+					wreg_o <= `WriteDisable; instvalid = `InstValid;
+					imm <= {20{inst_i[31]}, inst_i[31:25], inst_i[11:7]};
+					opcode_o <= `OP_STORE; wd_o <= `NOPRegAddr;
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b1;                       
+					case (op2)
+						`FUNCT3_SB: begin
+							func3_o <= `FUNCT3_SB;
+						end
+						`FUNCT3_SH: begin
+							func3_o <= `FUNCT3_SH;
+						end
+						`FUNCT3_SW: begin
+							func3_o <= `FUNCT3_SW;
+						end
+					endcase
+		  		end
+				`OP_OP_IMM: begin
+					wreg_o <= `WriteEnable; instvalid = `InstValid;
+					imm <= {21{inst_i[31]}, inst_i[30:20]};
+					opcode_o <= `OP_OP_IMM; wd_o <= inst_i[11:7];
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b0;
+					case (op2)
+						`FUNCT3_ADDI: begin
+							func3_o <= `FUNCT3_ADDI;
+						end
+						`FUNCT3_SLTI: begin
+							func3_o <= `FUNCT3_SLTI;
+						end
+						`FUNCT3_SLTIU: begin
+							func3_o <= `FUNCT3_SLTIU;
+						end
+						`FUNCT3_XORI: begin
+							func3_o <= `FUNCT3_XORI;
+						end
+						`FUNCT3_ORI: begin
+							func3_o <= `FUNCT3_ORI;
+						end
+						`FUNCT3_ANDI: begin
+							func3_o <= `FUNCT3_ANDI;
+						end
+						`FUNCT3_SLLI: begin
+							func3_o <= `FUNCT3_SLLI;
+						end
+						`FUNCT3_SRLI_SRAI: begin
+							func3_o <= `FUNCT3_SRLI_SRAI;
+							case (op3)
+								`FUNCT7_SRLI: begin
+									func7_o <= `FUNCT7_SRLI;
+								end
+								`FUNCT7_SRAI: begin
+									func7_o <= `FUNCT7_SRAI;
+								end
+							endcase
+						end
+					endcase
+				end
+				`OP_OP: begin
+					wreg_o <= `WriteEnable; instvalid = `InstValid;
+					imm <= {32'b0};
+					opcode_o <= `OP_OP; wd_o <= inst_i[11:7];
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b1;                       
+					case (op2)
+						`FUNCT3_ADD_SUB: begin
+							func3_o <= `FUNCT3_ADD_SUB;
+							case (op3)
+								`FUNCT7_ADD: begin
+									func7_o <= `FUNCT7_ADD;
+								end
+								`FUNCT7_SUB: begin
+									func7_o <= `FUNCT7_SUB;
+								end
+							endcase
+						end
+						`FUNCT3_SLL: begin
+							func3_o <= `FUNCT3_SLL;
+							func7_o <= `FUNCT7_SLL;
+						end
+						`FUNCT3_SLT: begin
+							func3_o <= `FUNCT3_SLT;
+							func7_o <= `FUNCT7_SLT;
+						end
+						`FUNCT3_SLTU: begin
+							func3_o <= `FUNCT3_SLTU;
+							func7_o <= `FUNCT7_SLTU;
+						end
+						`FUNCT3_XOR: begin
+							func3_o <= `FUNCT3_XOR;
+							func7_o <= `FUNCT7_XOR;
+						end
+						`FUNCT3_SRL_SRA: begin
+							func3_o <= `FUNCT3_SRL_SRA;
+							case (op3)
+								`FUNCT7_SRL: begin
+									func7_o <= `FUNCT7_ADD;
+								end
+								`FUNCT7_SRA: begin
+									func7_o <= `FUNCT7_SUB;
+								end
+							endcase
+						end
+						`FUNCT3_OR: begin
+							func3_o <= `FUNCT3_OR;
+							func7_o <= `FUNCT7_OR;
+						end
+						`FUNCT3_AND: begin
+							func3_o <= `FUNCT3_AND;
+							func7_o <= `FUNCT7_AND;
+						end
+					endcase
+		  		end
+				`OP_MISC_MEM: begin
+					wreg_o <= `WriteDisable; instvalid = `InstValid;
+					imm <= {32'b0};
+					opcode_o <= `OP_MISC_MEM; wd_o <= `NOPRegAddr
+					reg1_read_o <= 1'b0; reg2_read_o <= 1'b0;
+					case (op2)
+						`FUNCT3_FENCE: begin
+							func3_o <= `FUNCT3_FENCE;
+						end
+						`FUNCT3_FENCEI: begin
+							func3_o <= `FUNCT3_FENCEI
+						end
+					endcase
+				end
+		    	default: begin
+		    	end
+		  endcase		  //case op	
 		end    //if
 	end      //always
 
--- id.v
+++ id.v
@@ -1,41 +1,9 @@
-//////////////////////////////////////////////////////////////////////
-////                                                              ////
-//// Copyright (C) 2014 leishangwen@163.com                       ////
-////                                                              ////
-//// This source file may be used and distributed without         ////
-//// restriction provided that this copyright statement is not    ////
-//// removed from the file and that any derivative work contains  ////
-//// the original copyright notice and the associated disclaimer. ////
-////                                                              ////
-//// This source file is free software; you can redistribute it   ////
-//// and/or modify it under the terms of the GNU Lesser General   ////
-//// Public License as published by the Free Software Foundation; ////
-//// either version 2.1 of the License, or (at your option) any   ////
-//// later version.                                               ////
-////                                                              ////
-//// This source is distributed in the hope that it will be       ////
-//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
-//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
-//// PURPOSE.  See the GNU Lesser General Public License for more ////
-//// details.                                                     ////
-////                                                              ////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-// Module:  id
-// File:    id.v
-// Author:  Lei Silei
-// E-mail:  leishangwen@163.com
-// Description: 译码阶段
-// Revision: 1.0
-//////////////////////////////////////////////////////////////////////
-
 `include "defines.v"
 
 module id(
 
-	input wire										rst,
-	input wire[`InstAddrBus]			pc_i,
+	input wire					  rst,
+	input wire[`InstAddrBus]	  pc_i,
 	input wire[`InstBus]          inst_i,
 
 	input wire[`RegBus]           reg1_data_i,
@@ -48,54 +16,120 @@ module id(
 	output reg[`RegAddrBus]       reg2_addr_o, 	      
 	
 	//送到执行阶段的信息
-	output reg[`AluOpBus]         aluop_o,
-	output reg[`AluSelBus]        alusel_o,
+	output reg[`OpcodeBus]        opcode_o,
+	output reg[`Func3Bus]         func3_o,
+	output reg[`Func7Bus]         func7_o,
 	output reg[`RegBus]           reg1_o,
 	output reg[`RegBus]           reg2_o,
 	output reg[`RegAddrBus]       wd_o,
 	output reg                    wreg_o
+	
+	//送到pc_reg
+	output reg                    pc_sig,
+	output reg[`InstAddrBus]      pc_o,
 );
+  wire[6:0] op  = inst_i[6:0];
+  wire[2:0] op2 = inst_i[14:12];
+  wire[6:0] op3 = inst_i[31:25];
 
-  wire[5:0] op = inst_i[31:26];
-  wire[4:0] op2 = inst_i[10:6];
-  wire[5:0] op3 = inst_i[5:0];
-  wire[4:0] op4 = inst_i[20:16];
   reg[`RegBus]	imm;
   reg instvalid;
   
  
-	always @ (*) begin	
-		if (rst == `RstEnable) begin
-			aluop_o <= `EXE_NOP_OP;
-			alusel_o <= `EXE_RES_NOP;
-			wd_o <= `NOPRegAddr;
-			wreg_o <= `WriteDisable;
+	always @ (*) begin
+		//regfile
+		reg1_read_o <= 1'b0;
+		reg2_read_o <= 1'b0;
+		reg1_addr_o <= `NOPRegAddr;
+		reg2_addr_o <= `NOPRegAddr;	
+		//exec
+		instvalid <= `InstInvalid;
+		opcode_o <= `EXE_OP_NOP;
+		func7_o <= `EXE_FUNC7_NOP;
+		func3_o <= `EXE_FUNC3_NOP;
+		wd_o <= `NOPRegAddr;
+		wreg_o <= `WriteDisable;
+		imm <= `ZeroWord;
+		//pc_reg
+		pc_o <= `ZeroWord;
+		pc_sig <= 1'b0;
+		//begin decoding		
+		if (rst != `RstEnable) begin
 			instvalid <= `InstValid;
-			reg1_read_o <= 1'b0;
-			reg2_read_o <= 1'b0;
-			reg1_addr_o <= `NOPRegAddr;
-			reg2_addr_o <= `NOPRegAddr;
-			imm <= 32'h0;			
-	  end else begin
-			aluop_o <= `EXE_NOP_OP;
-			alusel_o <= `EXE_RES_NOP;
-			wd_o <= inst_i[15:11];
-			wreg_o <= `WriteDisable;
-			instvalid <= `InstInvalid;	   
-			reg1_read_o <= 1'b0;
-			reg2_read_o <= 1'b0;
-			reg1_addr_o <= inst_i[25:21];
-			reg2_addr_o <= inst_i[20:16];		
-			imm <= `ZeroWord;			
-		  case (op)
-		  	`EXE_ORI:			begin                        //ORI指令
-		  		wreg_o <= `WriteEnable;		aluop_o <= `EXE_OR_OP;
-		  		alusel_o <= `EXE_RES_LOGIC; reg1_read_o <= 1'b1;	reg2_read_o <= 1'b0;	  	
-					imm <= {16'h0, inst_i[15:0]};		wd_o <= inst_i[20:16];
-					instvalid <= `InstValid;	
-		  	end 							 
-		    default:			begin
-		    end
+			opcode_o <= op;
+		  	case (op)
+		  		`OP_LUI: begin                       
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+					imm <= {inst_i[31:12], 12'b0};
+		  		end
+				`OP_AUIPC: begin
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+					imm <= {inst_i[31:12], 12'b0} + pc_i - 4;		
+				end
+				`OP_JAL: begin                       
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+					imm <= pc_i - 4;
+
+					pc_o <= {11{inst_i[31]}, inst_i[19:12],
+							inst_i[20], inst_i[30:21], 1'b0} + pc_i - 4;
+					pc_sig <= 1'b1;
+		  		end
+				`OP_JALR: begin
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+					imm <= pc_i - 4;
+
+					pc_o <= ({20{inst_i[31]}, inst_i[30:20]} + reg1_data_i);
+					pc_sig <= 1'b1;
+					
+					func3_o <= `FUNCT3_JALR;
+					reg1_read_o <= 1'b1;
+				end
+				`OP_BRANCH: begin
+					pc_i <= {20{inst_i[31]}, inst_i[7],
+							inst_i[30:25], inst_i[11:8], 1'b0} + pc_i - 4;
+					pc_sig <= 1'b1;
+
+					func3_o <= op2; 
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b1;
+		  		end
+				`OP_LOAD: begin
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+					imm <= {20{inst_i[31]}, inst_i[30:20]};
+
+					func3_o <= op2;
+					reg1_read_o <= 1'b1; 
+				end
+				`OP_STORE: begin
+					imm <= {20{inst_i[31]}, inst_i[31:25], inst_i[11:7]};
+					
+					func3_o <= op2;
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b1;                     
+		  		end
+				`OP_OP_IMM: begin
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+					imm <= {21{inst_i[31]}, inst_i[30:20]};
+
+					func3_o <= op2;
+					reg1_read_o <= 1'b1;
+					case (op2)
+						`FUNCT3_SRLI_SRAI: begin
+							func7_o <= op3;
+						end
+						default: begin
+						end
+					endcase
+				end
+				`OP_OP: begin
+					wreg_o <= `WriteEnable; wd_o <= inst_i[11:7];
+
+					func3_o <= op2; func7_o <= op3;
+					reg1_read_o <= 1'b1; reg2_read_o <= 1'b1;
+		  		end
+				`OP_MISC_MEM: begin
+					func3_o <= op2;
+				end
+		    	default: begin
+		    	end
 		  endcase		  //case op			
 		end       //if
 	end         //always
@@ -104,25 +138,25 @@ module id(
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			reg1_o <= `ZeroWord;
-	  end else if(reg1_read_o == 1'b1) begin
-	  	reg1_o <= reg1_data_i;
-	  end else if(reg1_read_o == 1'b0) begin
-	  	reg1_o <= imm;
-	  end else begin
-	    reg1_o <= `ZeroWord;
-	  end
+	  	end else if(reg1_read_o == 1'b1) begin
+	  		reg1_o <= reg1_data_i;
+	  	end else if(reg1_read_o == 1'b0) begin
+	  		reg1_o <= imm;
+	  	end else begin
+	    	reg1_o <= `ZeroWord;
+	  	end
 	end
 	
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			reg2_o <= `ZeroWord;
-	  end else if(reg2_read_o == 1'b1) begin
-	  	reg2_o <= reg2_data_i;
-	  end else if(reg2_read_o == 1'b0) begin
-	  	reg2_o <= imm;
-	  end else begin
-	    reg2_o <= `ZeroWord;
-	  end
+	  	end else if(reg2_read_o == 1'b1) begin
+	  		reg2_o <= reg2_data_i;
+	  	end else if(reg2_read_o == 1'b0) begin
+	  		reg2_o <= imm;
+		end else begin
+			reg2_o <= `ZeroWord;
+		end
 	end
 
 endmodule
